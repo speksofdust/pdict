aad516617acf4e0b2e2dabca2c01c513d8cefdeb
Speks of Dust <speksofdust@users.noreply.github.com>
2016-02-18 13:34:18 -0500

    fixed docstrings and added some new features new features: 	PDict & ChildPDict: 		added a parseargs to __init__ (used as args for _parseinit) 		added an 'ifdata' option to __init__ (use parseinit='ifdata') 	added init_from_key_cls_pairs and init_from_key_cls_pairs_child

--- pdict.py
+++ pdict.py
@@ -18,35 +18,58 @@
 
 def init_from_key(d, key, cls, *args, **kwargs):
     d[key] = cls(d, args, kwargs)
-    
+
+def init_from_key_cls_pairs(d, key, cls):
+    for i in key_cls_pairs:
+        init_from_key(d, *key_cls_pairs[i])
+
 def init_from_key_child(d, key, cls, *args, **kwargs):
     """Same as 'init_from_key' but for when initalizing a ChildPDict object
     within a dict type object."""
-    d[key] = cls(d, d[key], args, kwargs)
+    d[key] = cls(d, args, kwargs)
 
+def init_from_key_cls_pairs_child(d, key_cls_pairs):
+    for i in key_cls_pairs:
+        init_from_key_child(d, *key_cls_pairs[i])
+
+
+__all__ = ["init_from_key", "init_from_key_cls_pairs", "init_from_key_child",
+    "init_from_key_cls_pairs_child"]
 
 class PDict(dict):
-    """Special dict class for storing nested data, with defaults and (optional)
-    automatic parsing on __init__.
-    
+    """Special dict class with defaults and (optional) automatic parsing on __init__. The aim is to make loading data into a dict who's values will
+    need to be stored in custom classes easier and simpler to initalize.
+
+    All the developer has to do is simply inherit their class from PDict and
+    override '_defaultdict to return a default dictionary (optional)
+    and then override _parseinit to control how data will be handled and loaded
+    into custom classes.
+
+
     An example to clarify usage.
     #Parsing data automatically into classes.
-    
+
     class Audio(dict): pass
     class Graphics(dict): pass
-    
+
     class ParseData(Pdict):
-    
+
         def _defaultdict(self):
             return {"audio": 0, "graphics": 0,}
-        
+
         def _parseinit(self):
             init_from_key(self, "audio", Audio)
             init_from_key(self, "graphics", Graphics)
+            # could also be done as
+            # init_from_key_cls_pairs(self, (
+                ("audio", Audio),
+                ("graphics", Graphics)
+                # if you have args or kwargs you would use
+                # ("graphics", Graphics, args_here, kwargs_here),
+                )
 
-
-    data = {'audio': 
-                {'volume: 21', 
+    data = {'audio':
+                {'volume: 21',
                 'panning': 0
                 },
             {'graphics':
@@ -54,24 +77,30 @@ class PDict(dict):
                 'bilinear-filtering': False
                 }
             }
-    
+
     pd = ParseData(data)
     pd
     {'audio': {'volume: 21', 'panning': 0}, {'graphics': {'quality': 'high',
         'bilinear-filtering': False}}
     """
     __slots__ = dict.__slots__
-    def __init__(data={}, parseinit=True):
+    def __init__(data={}, parseinit=True, parseargs=()):
+        # init the super class (dict) using _defaultdict
         super().__init__(self._defaultdict())
+
         self.update(data)
-        if parseinit: self._parseinit()
+
+        if parseinit == True:
+            if parseinit == 'ifdata':
+                 if data: self._parseinit(parseargs)
+            else: self._parseinit(parseargs)
 
     def _defaultdict(self):
         """Overload this to set the initial dict (initdict) that will be used
         for default data."""
         return {}
 
-    def _parseinit(self):
+    def _parseinit(self, *args):
         """Overload this to control data parsing when loading data at init."""
         pass
 
@@ -84,14 +113,14 @@ class PDict(dict):
         """The dict used to initalize and reset this object. Returns a new dict
         defined by '_defaultdict'."""
         return self._defaultdict
-    
+
     def modified_values(self):
         """Return an iterator of keys for values that differ from those in
         initvalues."""
         return iter(i for i in self.keys() if self[i] != self._defaultdict[i])
 
     def unmodified_values(self):
-        """Return an iterator of keys for values that match those in 
+        """Return an iterator of keys for values that match those in
         initvalues."""
         return iter(i for i in self.keys() if self[i] == self._defaultdict[i])
 
@@ -100,15 +129,13 @@ class PDict(dict):
         return any(self[i] != self._defaultdict[i] for i in self.keys())
 
     def has_new_keys(self):
-        """True if any keys are present that do not exist in initvalues.""" 
+        """True if any keys are present that do not exist in initvalues."""
         return iter(i for i in self.keys() if i not in self._defaultdict.keys()
 
 
 class ChildPDict(PDict):
-    """Same as PDict, except adds a parent attribute."""
+    """Same as PDict, except adds a '_parent' attribute."""
     __slots__ = PDict.__slots__ + "_parent"
-    def __init__(self, parent, data={}, parseinit=True):
+    def __init__(self, parent, data={}, parseinit=True, parseargs=()):
         self._parent = parent
-        super().__init__(self._defaultdict())
-        self.update(data)
-        if parseinit: self._parseinit()
+        super().__init__(data=data, parseinit=parseinit, parseargs=parseargs)
